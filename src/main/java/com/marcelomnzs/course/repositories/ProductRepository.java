5b0ccb7214fb66b08a3382f0d01073ae27c417b9
Marcelo Antonio <marceloa.mnzs@gmail.com>
2025-09-10 17:12:45 -0300

    Product Entity

---
+++ src/main/java/com/marcelomnzs/course/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.marcelomnzs.course.repositories;
+
+import com.marcelomnzs.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/com/marcelomnzs/course/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.marcelomnzs.course.services;
+
+import com.marcelomnzs.course.entities.Product;
+import com.marcelomnzs.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> product = repository.findById(id);
+        return product.get();
+    }
+}
